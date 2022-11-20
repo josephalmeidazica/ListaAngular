e3383e44e618159106e18095251f97c5380d3141
Joseph Zica <josephalmedazica@gmail.com>
2022-11-20 20:56:41 -0300

    feature: calculadora e tabuada

--- lista-angular/src/app/componentes/tabuada/tabuada.component.ts
+++ lista-angular/src/app/componentes/tabuada/tabuada.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-tabuada',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabuada.component.css']
 })
 export class TabuadaComponent implements OnInit {
+  tabuadaForm = this.formBuilder.group({
+    numero: 0
+  });
 
-  constructor() { }
+  tabuada = '';
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    for(var i = 0;i<10;i++){
+      var numeroAtual = this.tabuadaForm.value.numero;
+      var resultado = numeroAtual * i;
+      this.tabuada += `${i} x ${numeroAtual} = ${resultado} <br>`
+    }
+  }
 
   ngOnInit(): void {
   }
